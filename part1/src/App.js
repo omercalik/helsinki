bf13a107b8de5d2a180eb0e33f12c7b3b07b39ab
vignerd <omerbcalik@gmail.com>
2021-03-20 00:39:02 +0300

    part1

---
+++ part1/src/App.js
@@ -0,0 +1,20 @@
+import Header from "./Header"
+import Content from "./Content"
+import Total from "./Total"
+
+function App() {
+  const course = "Half Stack application development"
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
